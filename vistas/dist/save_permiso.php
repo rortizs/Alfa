22a04099d82ca6b2e88990be216f54d17d8fd0ac
zayra-del <zmoscosoc@miumg.edu.gt>
2020-05-13 13:22:10 -0600

    permisos

---
+++ vistas/dist/save_permiso.php
@@ -0,0 +1,25 @@
+<?php
+
+include ("db_user.php");
+
+
+
+if (isset($_POST['save_permiso'])){
+     $id = $_POST['id'];
+     $nombre_permiso = $_POST['nombre_permiso'];
+
+
+     $query = "INSERT INTO  permisos(id, nombre_permiso) 
+    VALUES ('$id', '$nombre_permiso')";
+     $result = mysqli_query ($conn, $query);
+     if (!$result){
+         die("Query Failed");
+     }
+
+      $_SESSION['message'] = 'Permiso Guardado';
+      $_SESSION['message_type'] = 'info';
+
+      header ("Location: crearpermisos.html");
+}
+
+?>
