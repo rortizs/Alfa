7f27c558c8d7ee1251fe4f29bfe190ac7623dd66
zayra-del <zmoscosoc@miumg.edu.gt>
2020-05-15 23:01:44 -0600

    permiso usuario
    arreglos en la tabla permiso usuario.

---
+++ modelos/save_permisousuario.php
@@ -0,0 +1,25 @@
+<?php
+
+include ("db_user.php");
+
+
+if (isset($_POST['save_permisousuario'])){
+        $id_usuario = $_POST['id_usuario'];
+        $id_permiso = $_POST['id_permiso'];
+
+
+
+        $query = "INSERT INTO  permisos_usuario (id_usuario, id_permiso) 
+        VALUES ('$id_usuario', $id_permiso)";
+         $result = mysqli_query ($conn, $query);
+         if (!$result){
+             die("Query Failed");
+         }
+
+         $_SESSION['message'] = 'Permiso Usuario Guardado';
+         $_SESSION['message_type'] = 'info';
+   
+         header ("Location: ../vistas/crearpermisousuario.html");
+   }
+   
+   ?>
