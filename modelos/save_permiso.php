d20071e5987aa414f7631ad95f8e7569904b5f30
zayra-del <zmoscosoc@miumg.edu.gt>
2020-05-26 10:46:44 -0600

    permiso usuario

--- modelos/save_permiso.php
+++ modelos/save_permiso.php
@@ -7,7 +7,6 @@ if (isset($_POST['save_permiso'])){
         $nombre_permiso = $_POST['nombre_permiso'];
 
 
-
         $query = "INSERT INTO  permisos(nombre_permiso) 
         VALUES ('$nombre_permiso')";
          $result = mysqli_query ($conn, $query);
@@ -15,7 +14,7 @@ if (isset($_POST['save_permiso'])){
              die("Query Failed");
          }
 
-         $_SESSION['message'] = 'Usuario Guardado';
+         $_SESSION['message'] = 'Permiso Guardado';
          $_SESSION['message_type'] = 'info';
    
          header ("Location: ../vistas/crearpermisos.html");
--- vistas/crearpermisousuario.php
+++ vistas/crearpermisousuario.php
@@ -1,6 +1,6 @@
 <?php include ("../modelos/db_user.php")
 ?>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="utf-8" />
@@ -9,10 +9,11 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Detalles MH</title>
-    <link rel="icon" type="image/png" href="../public/assets/img/globo-title.png"/> 
+    <link rel="icon" type="image/png" href="../public/assets/img/globo-title.png"/>
     <link href="../public/css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" >
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous">
     </script>
 </head>
@@ -103,34 +104,32 @@
                                     Producto</a><a class="nav-link" href="tablaproducto.php"><i class="fas fa-table"></i>Tabla Producto</a></nav>
                         </div>
                     </div>
-                    <div class="sb-sidenav-footer">
+                    <div style="position: absolute;bottom: 0;width: -webkit-fill-available;" class="sb-sidenav-footer">
                         DETALLES MH
                     </div>
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4"> <i class="fas fa-clipboard-check"></i>Crear Permiso Usuario</h1>
+                    <hr>
                     <div class="container-fluid">
-                        <h1 class="mt-4">DETALLES MH</h1>
-
-                        <br></br>
-
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-md-10">
-    
-                                    <?php if (isset($_SESSION['message'])){ ?>
-                                    <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show"
-                                        role="alert">
-                                        <?= $_SESSION['message'] ?>
-                                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <?php session_unset();} ?>
-                                    <div class="card card-body">
-                                        <form action="../modelos/save_permisousuario.php" method="POST">
+                        <div class="row">
+                            <div class="col-md-12">
+                               <!-- <?php if (isset($_SESSION['message'])){ ?>
+                                <div class="alert alert-<?= $_SESSION['message_type'];?> alert-dismissible fade show"
+                                    role="alert">
+                                    <?= $_SESSION['message'] ?>
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <?php session_unset();} ?> -->
+                                
+                                <div class="card card-body">
+                                    <form action="../modelos/save_permisousuario.php" method="POST" enctype="multipart/form-data">
                                             <div class="col-md-8 order-md-1">
                                                 <h4 class="mb-3"><i class="fas fa-male"></i>Ingresar datos para la creación del permiso </h4>
                                                 <form class="needs-validation" novalidate>
@@ -214,7 +213,7 @@
                                                       </button>
                                                     </div>
                                                     <div class="modal-body">
-                                                        ¿Desea guardar la Categoría?
+                                                        ¿Desea guardar el Permiso Usuario?
                                                     </div>
                                                     <div class="modal-footer">
                                                    
@@ -231,19 +230,24 @@
                                             </div>
     
                                     </div>
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-    <script src="../public/js/scripts.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous">
-    </script>
-    <script src="../public/assets/demo/chart-area-demo.js"></script>
-    <script src="../public/assets/demo/chart-bar-demo.js"></script>
-    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous">
-    </script>
-    <script src="../public/assets/demo/datatables-demo.js"></script>
+
+
+                        
+                            <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+                            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+                                crossorigin="anonymous"></script>
+                            <script src="../public/js/scripts.js"></script>
+                            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"
+                                crossorigin="anonymous">
+                            </script>
+                            <script src="../public/assets/demo/chart-area-demo.js"></script>
+                            <script src="../public/assets/demo/chart-bar-demo.js"></script>
+                            <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"
+                                crossorigin="anonymous"></script>
+                            <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"
+                                crossorigin="anonymous">
+                            </script>
+                            <script src="../public/assets/demo/datatables-demo.js"></script>
 </body>
 
 </html>
