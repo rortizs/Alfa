419062293ac93c27efee5d20a595ea2f3767c764
zayra-del <zmoscosoc@miumg.edu.gt>
2020-05-26 18:53:48 -0600

    ruta producto
    se arreglo la ruta de la tabla producto

--- vistas/home.php
+++ vistas/home.php
@@ -72,6 +72,14 @@
                             <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="crearpermisousuario.php">Permiso de Usuarios</a></nav>
                             </div>
+                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts"
+                            >
+                            Producto
+                            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div
+                        ></a>
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav"><a class="nav-link" href="crearproductos.php">Crear Productos</a><a class="nav-link" href="tablaproducto.php">Tabla Producto</a></nav>
+                        </div> 
                     </div>
                     <div class="sb-sidenav-footer">
                         <div class="small"></div>
